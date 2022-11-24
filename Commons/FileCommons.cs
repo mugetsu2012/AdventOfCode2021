4744c0a8c442283c824701fe9091fff8c54569e0
Douglas Ortiz <dpaniagua@affinitiv.com>
2022-11-24 14:36:46 -0600

    Adding day 1 solving!

---
+++ Commons/FileCommons.cs
@@ -0,0 +1,17 @@
+﻿namespace Commons
+{
+    public static class FileCommons
+    {
+        public static async Task<string> ReadTextFile(string filePath)
+        {
+            return await File.ReadAllTextAsync(filePath);
+        }
+
+        public static async Task<int[]> ReadTextAsIntegerArray(string filePath)
+        {
+            string[] lines = await File.ReadAllLinesAsync(filePath);
+
+            return Array.ConvertAll(lines, x => int.Parse(x));            
+        }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Commons;
+
+Console.WriteLine("Hello, World!");
+
+// We read the input
+int[] input = await FileCommons.ReadTextAsIntegerArray("Day1Input.txt");
+int largerCount = 0;
+
+for(int i = 1; i < input.Length; i++)
+{
+    // We compare the current value with the previous value
+    if (input[i] > input[i - 1])
+    {
+        largerCount ++;
+    }
+}
+
+Console.WriteLine($"There are {largerCount} measurements that show increasing");
