18a0d7904e7efa04dca2b09112f08a82ae43b815
Douglas Ortiz <dpaniagua@affinitiv.com>
2022-11-24 23:28:42 -0600

    Adding day 2 code

---
+++ Day2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Commons;
+
+// We read the lines
+string[] lines = await File.ReadAllLinesAsync("Day2Input.txt");
+
+int currentHorizontalPos = 0;
+int currentDepth = 0;
+
+// We loop and evaluate each line
+for(int i = 0; i < lines.Length; i++)
+{
+    // We splipt the line into an array of string. We know that the format is like: forward 1
+    string[] parts = lines[i].Split(' ');
+
+    // We evaluate the direction
+    switch (parts[0])
+	{
+		case "forward":
+			currentHorizontalPos += int.Parse(parts[1]);
+			break;
+
+		case "up":
+			currentDepth -= int.Parse(parts[1]);
+			break;
+
+		case "down":
+            currentDepth += int.Parse(parts[1]);
+            break;
+
+		default:
+			break;
+	}
+
+}
+
+
+Console.WriteLine($"The current horizontal position is: {currentHorizontalPos} and the current depth is: {currentDepth}");
+Console.WriteLine($"The multiply of these is: {currentDepth*currentHorizontalPos}");
+
